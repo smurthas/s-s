34641936ddd1a555021b28ca4a54c2c8faafac3d
Simon Murtha-Smith <simon@murtha-smith.com>
2013-01-20 14:31:21 -0800

    color changes and lots of style cleanup

--- js/main.js
+++ js/main.js
@@ -1,40 +1,23 @@
-  $(document).ready(function() {
+var SWITCH_POS = 200;
+// scrolling behavior function
+$(window).scroll(function () {
 
-    // center entry landing and size (?)
-    // CODE NEEDED
+  var pos = $("body").scrollTop();
 
-    // set deadspace to height of screen
-//    $('#deadspace').css('height', 100);
- //     $('#nav').fadeIn();
-  });
-
-
-  var SWITCH_POS = 200;
-  // scrolling behavior function
-  $(window).scroll(function () {
-
-    var pos = $("body").scrollTop();
-
-    if (pos < SWITCH_POS){
-
-      if ( $('#nav').is(':visible')){
-        $('#nav').fadeOut();
-      }
+  if (pos < SWITCH_POS){
+    if ( $('#nav').is(':visible')){
+      $('#nav').fadeOut();
     }
-
-
-    if (pos > SWITCH_POS){
-      if ( $('#entry_landing').is(':visible')){
-        $('#nav').fadeIn();
-      }
+  } else {
+    if ( $('#entry_landing').is(':visible')){
+      $('#nav').fadeIn();
     }
+  }
 
-    // set opacity of entry_landing
-    $('#entry_landing').css('opacity', 1-(pos/SWITCH_POS));
+  // set opacity of entry_landing
+  $('#entry_landing').css('opacity', 1-(pos/SWITCH_POS));
 
-  //console.log( $("body").scrollTop() );
-
-  });
+});
 
 // jump function to animate smoothly
 function jump(id){
@@ -42,19 +25,13 @@ function jump(id){
   // calculate where to scroll to
   var offset = $(id).offset().top - $('#nav').outerHeight() - 10;
 
-  //console.log(offset);
   $('body').animate({scrollTop: offset}, 400);
 }
 
 $(function(){
-//				$('#slides').slides({
-//					generateNextPrev: true
-//				});
-
 
   $('.slides').slides({
     preload: true,
     generateNextPrev: true
   });
-
 });
